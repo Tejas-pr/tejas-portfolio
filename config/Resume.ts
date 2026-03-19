2ca6e6c6dc7d84a52b8dfac4092c48bc121185e5
Tejas <tejas.teju02@gmail.com>
2026-03-19 12:10:15 +0530

    resume update

--- config/Resume.ts
+++ config/Resume.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1lmqBR71u-RXf5Ub9OYa_B6XvYRb-WuHs/preview',
+  url: 'https://drive.google.com/file/d/1LnLCPVlWKaleXphFYWmvFKNWQRkZUHfj/preview',
 };
