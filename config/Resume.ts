bda296816413019abdc1af28004243b96ade11d3
Tejas <tejas.teju02@gmail.com>
2026-03-22 15:15:30 +0530

    dynamic resume

--- config/Resume.ts
+++ config/Resume.ts
@@ -1,3 +1,9 @@
+const fallbackUrl = 'https://drive.google.com/file/d/1fT0X918j_0F-GasNU1QRn5WwmdIiR-nS/preview';
+
+const shareLink = process.env.UNIQUE_SHARE_CODE;
+
+const resumeLink = process.env.RESUME_LINK || (shareLink ? `https://drive.google.com/file/d/${shareLink}/preview` : fallbackUrl);
+
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1LnLCPVlWKaleXphFYWmvFKNWQRkZUHfj/preview',
+  url: resumeLink,
 };
